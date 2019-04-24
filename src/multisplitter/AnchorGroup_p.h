0a3f6a5b62bc6c3c372a918eb50f32b503c33d53
Allen Winter <allen.winter@kdab.com>
2019-04-24 13:14:56 -0400

    LayoutSaver.cpp, AnchorGroup* - add license+copyright header

--- src/multisplitter/AnchorGroup_p.h
+++ src/multisplitter/AnchorGroup_p.h
@@ -1,3 +1,23 @@
+/*
+  This file is part of KDDockWidgets.
+
+  Copyright (C) 2018-2019 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Author: Sérgio Martins <sergio.martins@kdab.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef KD_MULTISPLITTER_ANCHORGROUP_P_H
 #define KD_MULTISPLITTER_ANCHORGROUP_P_H
 
