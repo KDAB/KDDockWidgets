fd5b2ad2f136cb4c2b2c1e9d9014333c6ce929c2
Sergio Martins <sergio.martins@kdab.com>
2019-07-20 17:25:25 +0100

    Use AA_UseHighDpiPixmaps in the example
    it's a best practice

--- examples/basic/main.cpp
+++ examples/basic/main.cpp
@@ -90,6 +90,7 @@ public:
 int main(int argc, char **argv)
 {
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QApplication app(argc, argv);
     app.setOrganizationName(QStringLiteral("KDAB"));
     app.setApplicationName(QStringLiteral("Test app"));
