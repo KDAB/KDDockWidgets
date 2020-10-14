697c140883cee0cbee7c44741f38ea8e2dc5abfc
Allen Winter <allen.winter@kdab.com>
2020-10-14 10:07:48 -0400

    set version to 1.1.0

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -11,7 +11,7 @@ from conans import ConanFile, CMake, tools
 
 class KDDockWidgetsConan(ConanFile):
     name = "kddockwidgets"
-    version = "1.0.0"
+    version = "1.1.0"
     default_user = "kdab"
     default_channel = "stable"
     license = ("https://raw.githubusercontent.com/KDAB/KDDockWidgets/master/LICENSES/GPL-2.0-only.txt",
