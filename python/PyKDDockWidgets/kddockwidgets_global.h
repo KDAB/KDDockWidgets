14358dd1535eb0bb825986a55697905fe7161ab3
Renato Araujo Oliveira Filho <renato.araujo@kdab.com>
2020-12-15 09:43:10 -0300

    Fixed python bindings build

--- python/PyKDDockWidgets/kddockwidgets_global.h
+++ python/PyKDDockWidgets/kddockwidgets_global.h
@@ -14,10 +14,11 @@
 // Make "signals:", "slots:" visible as access specifiers
 #define QT_ANNOTATE_ACCESS_SPECIFIER(a) __attribute__((annotate(#a)))
 
+#define QT_WIDGETS_LIB
 // Define PYTHON_BINDINGS this will be used in some part of c++ to skip problematic parts
 #define PYTHON_BINDINGS
 
-#include <kddockwidgets/MainWindowBase.h>
-#include <kddockwidgets/MainWindow.h>
-#include <kddockwidgets/DockWidgetBase.h>
-#include <kddockwidgets/DockWidget.h>
+#include <DockWidgetBase.h>
+#include <MainWindowBase.h>
+#include <MainWindow.h>
+#include <DockWidget.h>
