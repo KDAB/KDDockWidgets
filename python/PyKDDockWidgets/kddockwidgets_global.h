3ee15d1307356d18879e65e8633becba5fb95327
Sergio Martins <sergio.martins@kdab.com>
2020-12-15 12:15:23 +0000

    Give python a bit of help building KDDW

--- python/PyKDDockWidgets/kddockwidgets_global.h
+++ python/PyKDDockWidgets/kddockwidgets_global.h
@@ -17,6 +17,10 @@
 // Define PYTHON_BINDINGS this will be used in some part of c++ to skip problematic parts
 #define PYTHON_BINDINGS
 
+#ifndef QT_WIDGETS_LIB
+# define QT_WIDGETS_LIB
+#endif
+
 #include <kddockwidgets/MainWindowBase.h>
 #include <kddockwidgets/MainWindow.h>
 #include <kddockwidgets/DockWidgetBase.h>
