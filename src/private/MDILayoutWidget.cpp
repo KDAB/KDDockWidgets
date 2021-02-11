3c95081aee3c912be2ec39317c80190f8f1aa6f3
Sergio Martins <sergio.martins@kdab.com>
2021-02-11 19:37:05 +0000

    MDI: Support internal resize
    You can now resize docked MDI widgets

--- src/private/MDILayoutWidget.cpp
+++ src/private/MDILayoutWidget.cpp
@@ -47,8 +47,10 @@ void MDILayoutWidget::addDockWidget(DockWidgetBase *dw, QPoint localPt)
         newItem->setGuestWidget(frame);
     } else {
         frame = Config::self().frameworkWidgetFactory()->createFrame();
-        newItem->setGuestWidget(frame);
+        frame->setAllowedResizeSides(CursorPosition_All);
         frame->addWidget(dw);
+
+        newItem->setGuestWidget(frame);
     }
 
     Q_ASSERT(!newItem->geometry().isEmpty());
