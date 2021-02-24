a6f19e07c4abf357a63fb99f3ecbbafaa278ed03
Sergio Martins <sergio.martins@kdab.com>
2021-02-24 13:49:26 +0000

    Fixed Flag_NativeTitleBar not hidding the client title bars
    When restoring the layout the native title bar would be visible
    Fixes #170

--- src/private/FloatingWindow.cpp
+++ src/private/FloatingWindow.cpp
@@ -412,6 +412,9 @@ void FloatingWindow::updateTitleBarVisibility()
 
     bool visible = true;
 
+    for (Frame *frame : frames())
+        frame->updateTitleBarVisibility();
+
     if (KDDockWidgets::usesClientTitleBar()) {
         const auto flags = Config::self().flags();
         if ((flags & Config::Flag_HideTitleBarWhenTabsVisible) && !(flags & Config::Flag_AlwaysTitleBarWhenFloating)) {
@@ -419,9 +422,6 @@ void FloatingWindow::updateTitleBarVisibility()
                 visible = !frames().first()->hasTabsVisible();
             }
         }
-
-        for (Frame *frame : frames())
-            frame->updateTitleBarVisibility();
 
         m_titleBar->updateButtons();
     } else {
