4d6c3dac3d2f9a87a06306a0e98e4be8b94eed34
Sergio Martins <sergio.martins@kdab.com>
2021-02-26 16:58:07 +0000

    qtquick: Warn there's no QQmlEngine set

--- src/Config.cpp
+++ src/Config.cpp
@@ -235,6 +235,10 @@ void Config::setQmlEngine(QQmlEngine *qmlEngine)
 
 QQmlEngine *Config::qmlEngine() const
 {
+    if (!d->m_qmlEngine) {
+        qWarning() << "Please call KDDockWidgets::Config::setQmlEngine(engine)";
+    }
+
     return d->m_qmlEngine;
 }
 #endif
