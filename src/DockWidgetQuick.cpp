0d62c43b9edcba218ffaa8d95a958f8e1ab47a06
Sergio Martins <sergio.martins@kdab.com>
2021-03-01 19:58:00 +0000

    qtquick: keep the original parent of the guest around
    For debug purposes

--- src/DockWidgetQuick.cpp
+++ src/DockWidgetQuick.cpp
@@ -75,6 +75,11 @@ void DockWidgetQuick::setWidget(QWidgetAdapter *widget)
 void DockWidgetQuick::setWidget(QQuickItem *guest)
 {
     auto adapter = new QWidgetAdapter(this);
+
+    // In case the user app needs to use them:
+    adapter->setProperty("originalParent", QVariant::fromValue(guest->parent()));
+    adapter->setProperty("originalParentItem", QVariant::fromValue(guest->parentItem()));
+
     guest->setParentItem(adapter);
     guest->setParent(adapter);
     QWidgetAdapter::makeItemFillParent(guest);
