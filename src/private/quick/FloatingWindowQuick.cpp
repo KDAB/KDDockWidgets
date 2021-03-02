3fe44ee735c0dd6426f666296858dd85478e1cbd
Sergio Martins <sergio.martins@kdab.com>
2021-03-02 12:19:40 +0000

    qtquick: Enable transparency for the floating window
    You can now have windows with round corners

--- src/private/quick/FloatingWindowQuick.cpp
+++ src/private/quick/FloatingWindowQuick.cpp
@@ -33,6 +33,7 @@ public:
         : QQuickView(Config::self().qmlEngine(), nullptr)
         , m_floatingWindow(floatingWindow)
     {
+        setColor(QColor(Qt::transparent));
 
         updateSize();
 
