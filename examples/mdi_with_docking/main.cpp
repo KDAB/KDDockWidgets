5b87fb4435a901b2709c2d7e87ce1a00b6398b21
Sergio Martins <sergio.martins@kdab.com>
2022-01-07 17:31:20 +0000

    Add an examples/mdi_with_docking/ example

---
+++ examples/mdi_with_docking/main.cpp
@@ -0,0 +1,77 @@
+/*
+  This file is part of KDDockWidgets.
+
+  SPDX-FileCopyrightText: 2019-2021 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+  Author: Sérgio Martins <sergio.martins@kdab.com>
+
+  SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+
+#include "MyWidget.h"
+
+#include <kddockwidgets/DockWidget.h>
+#include <kddockwidgets/MainWindow.h>
+#include <kddockwidgets/MDIArea.h>
+
+#include <QStyleFactory>
+#include <QApplication>
+
+// clazy:excludeall=qstring-allocations
+
+int main(int argc, char **argv)
+{
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
+    QApplication app(argc, argv);
+
+    app.setOrganizationName(QStringLiteral("KDAB"));
+    app.setApplicationName(QStringLiteral("App supporting both docking and a MDI area"));
+
+    // Fusion looks better in general, but feel free to change
+    qApp->setStyle(QStyleFactory::create(QStringLiteral("Fusion")));
+
+    // # 1. Create our main window
+
+    KDDockWidgets::MainWindow mainWindow(QStringLiteral("MyMainWindow"), KDDockWidgets::MainWindowOption_HasCentralWidget);
+    mainWindow.setWindowTitle("Main Window");
+    mainWindow.resize(1600, 1200);
+    mainWindow.show();
+
+    // # 2. Create a dock widget, it needs a unique name
+    auto dock1 = new KDDockWidgets::DockWidget(QStringLiteral("MyDock1"));
+    auto widget1 = new MyWidget1();
+    dock1->setWidget(widget1);
+
+    auto dock2 = new KDDockWidgets::DockWidget(QStringLiteral("MyDock2"));
+    auto widget2 = new MyWidget2();
+    dock2->setWidget(widget2);
+
+    // # 3. Dock them
+    mainWindow.addDockWidget(dock1, KDDockWidgets::Location_OnLeft, nullptr, KDDockWidgets::InitialOption(QSize(300, 0)));
+    mainWindow.addDockWidget(dock2, KDDockWidgets::Location_OnBottom, nullptr, KDDockWidgets::InitialOption(QSize(0, 300)));
+
+
+    // 4. Create our MDI widgets, which will go into the MDI area
+    auto mdiWidget1 = new KDDockWidgets::DockWidget(QStringLiteral("MDI widget1"));
+    mdiWidget1->setWidget(new MyWidget1());
+
+    auto mdiWidget2 = new KDDockWidgets::DockWidget(QStringLiteral("MDI widget2"));
+    mdiWidget2->setWidget(new MyWidget2());
+
+    auto mdiWidget3 = new KDDockWidgets::DockWidget(QStringLiteral("MDI widget3"));
+    mdiWidget3->setWidget(new MyWidget3());
+
+    auto mdiArea = new KDDockWidgets::MDIArea();
+    mainWindow.setPersistentCentralWidget(mdiArea);
+
+    mdiArea->addDockWidget(mdiWidget1, QPoint(10, 10));
+    mdiArea->addDockWidget(mdiWidget2, QPoint(50, 50));
+    mdiArea->addDockWidget(mdiWidget3, QPoint(110, 110));
+
+
+    return app.exec();
+}
