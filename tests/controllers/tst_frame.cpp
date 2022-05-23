87e9cbc3889e3dc6cdccd0406d9b7b3b45a7efcf
Sergio Martins <sergio.martins@kdab.com>
2022-05-23 17:13:08 +0100

    tests: Add tst_frame

---
+++ tests/controllers/tst_frame.cpp
@@ -0,0 +1,24 @@
+/*
+  This file is part of KDDockWidgets.
+
+  SPDX-FileCopyrightText: 2019-2022 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+  Author: Sérgio Martins <sergio.martins@kdab.com>
+
+  SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+
+#include "../main.h"
+#include "controllers/Frame.h"
+
+#include "qtquick/Platform_qtquick.h"
+
+#include <QQmlContext>
+#include <QQmlEngine>
+
+TEST_CASE("Frame ctor")
+{
+    auto frame = new Controllers::Frame(nullptr, {});
+    delete frame;
+}
