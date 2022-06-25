a74d2aae7a97349493fd7bb5545af9c7bf78e075
Sergio Martins <sergio.martins@kdab.com>
2022-06-25 18:52:36 +0100

    tests: Add initial test for Window

---
+++ tests/tst_window.cpp
@@ -0,0 +1,21 @@
+/*
+  This file is part of KDDockWidgets.
+
+  SPDX-FileCopyrightText: 2019-2022 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+  Author: Sérgio Martins <sergio.martins@kdab.com>
+
+  SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+
+#include "main.h"
+#include "private/View_p.h"
+
+TEST_CASE("Window CTOR")
+{
+    auto rootView = createViewAndWindow({});
+    auto window = rootView->window();
+
+    REQUIRE(window);
+}
