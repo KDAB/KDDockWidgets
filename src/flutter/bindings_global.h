4b36717630f417c76009f004a34cc41224952522
Sergio Martins <sergio.martins@kdab.com>
2022-07-22 16:20:21 +0100

    flutter: Add binding generation support through dartagnan
    For now, only generating a few classes. We'll build up slowly.

---
+++ src/flutter/bindings_global.h
@@ -0,0 +1,20 @@
+/*
+  This file is part of KDDockWidgets.
+
+  SPDX-FileCopyrightText: 2019-2022 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+  Author: Sérgio Martins <sergio.martins@kdab.com>
+
+  SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+
+
+#include "Platform_flutter.h"
+#include "ViewFactory_flutter.h"
+#include "views/View_flutter.h"
+#include "../KDDockWidgets.h"
+
+// TODO: kdbindings is choking shiboken
+// #include "Window_flutter.h"
+// #include "../private/multisplitter/Item_p.h"
