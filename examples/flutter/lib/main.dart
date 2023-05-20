39c95124c5652f3ef6b134dd97211dd19f99782f
Sergio Martins <sergio.martins@kdab.com>
2023-05-20 19:46:11 +0100

    flutter: example now uses MainWindow
    It's a bit buggy, investigating

--- examples/flutter/lib/main.dart
+++ examples/flutter/lib/main.dart
@@ -11,6 +11,7 @@
 
 import 'package:KDDockWidgets/DockWidget.dart';
 import 'package:KDDockWidgets/DropArea.dart';
+import 'package:KDDockWidgets/MainWindow.dart';
 import 'package:KDDockWidgets/PositionedWidget.dart';
 import 'package:flutter/material.dart' hide View;
 import 'package:KDDockWidgetsBindings/Bindings_KDDWBindingsCore.dart'
@@ -24,6 +25,8 @@ import 'package:KDDockWidgets/Platform.dart' as KDDW;
 import 'package:KDDockWidgetsBindings/Bindings.dart' as KDDWBindings;
 
 void main() {
+  KDDWBindings.initFrontend(KDDWBindings.KDDockWidgets_FrontendType.Flutter);
+
   runApp(const MyApp());
 }
 
@@ -34,7 +37,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demos',
+      title: 'KDDockWidgets minimal example',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -54,70 +57,49 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late KDDWBindingsCore.DropArea dropArea;
-  late Widget dropAreaWidget;
+  late MainWindow mainWindow;
 
   _MyHomePageState() {
     KDDW.Platform();
-    dropArea = KDDWBindingsCore.DropArea(null,
-        KDDWBindings.KDDockWidgets_MainWindowOption.MainWindowOption_None);
 
-    final dropAreaView =
-        KDDWBindingsFlutter.View.fromCache(dropArea.view().thisCpp)
-            as KDDW.View;
-
-    KDDWBindingsCore.View.fromCppPointer(dropArea.view().thisCpp);
-    dropAreaView.setObjectName("droparea");
-
-    dropAreaView.m_fillsParent = true;
-    dropAreaWidget = dropAreaView.flutterWidget;
-
-    final group1 = KDDWBindingsCore.Group(parent: null);
-    final group2 = KDDWBindingsCore.Group(parent: null);
-    final group3 = KDDWBindingsCore.Group(parent: null);
-
-    dropArea.addWidget(
-        group1.view(), KDDWBindings.KDDockWidgets_Location.Location_OnLeft,
-        relativeTo: null, option: KDDWBindings.InitialOption());
-
-    dropArea.addWidget(
-        group2.view(), KDDWBindings.KDDockWidgets_Location.Location_OnRight,
-        relativeTo: null, option: KDDWBindings.InitialOption());
-
-    dropArea.addWidget(
-        group3.view(), KDDWBindings.KDDockWidgets_Location.Location_OnBottom,
-        relativeTo: null, option: KDDWBindings.InitialOption());
+    mainWindow = MainWindow("MyMainWindow", parent: null);
 
     final dw1 = DockWidget("dw1");
     final dw2 = DockWidget("dw2");
     final dw3 = DockWidget("dw3");
-    dw3.dockWidget().setFloating(true);
-    dw3.dockWidget().open();
 
-    dropArea.addDockWidget(dw1.dockWidget(),
-        KDDWBindings.KDDockWidgets_Location.Location_OnRight, null,
+    mainWindow.asMainWindowController().addDockWidget(
+        dw1.asDockWidgetController(),
+        KDDWBindings.KDDockWidgets_Location.Location_OnLeft,
+        relativeTo: null,
         initialOption: KDDWBindings.InitialOption());
 
-    // dw1.dockWidget().addDockWidgetAsTab(dw2.dockWidget(),
-    //     initialOption: KDDWBindings.InitialOption());
+    mainWindow.asMainWindowController().addDockWidget(
+        dw2.asDockWidgetController(),
+        KDDWBindings.KDDockWidgets_Location.Location_OnRight,
+        relativeTo: null,
+        initialOption: KDDWBindings.InitialOption());
+
+    mainWindow.asMainWindowController().addDockWidget(
+        dw3.asDockWidgetController(),
+        KDDWBindings.KDDockWidgets_Location.Location_OnBottom,
+        relativeTo: null,
+        initialOption: KDDWBindings.InitialOption());
   }
 
-  void _incrementCounter() {
-    dropArea.dumpLayout();
-    print("Layout out equally");
-    dropArea.layoutEqually();
-    print("Layout out equally done");
-    dropArea.dumpLayout();
-  }
+  void _incrementCounter() {}
 
   @override
   Widget build(BuildContext context) {
+    print("built");
     final windowOverlay = KDDW.WindowOverlayWidget();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(child: Stack(children: [dropAreaWidget, windowOverlay])),
+      body: Center(
+          child: Stack(children: [mainWindow.flutterWidget, windowOverlay])),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
