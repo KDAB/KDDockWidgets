5b4787dfbedfbb5cea433617f5f27a1cb3eaaab6
Sergio Martins <sergio.martins@kdab.com>
2023-05-25 18:09:12 +0100

    flutter: Implemented View::tabAt()
    Means tabs can detach now

--- src/flutter/dart/lib/TabBar.dart
+++ src/flutter/dart/lib/TabBar.dart
@@ -11,6 +11,7 @@
 
 import 'package:KDDockWidgets/View_mixin.dart';
 import 'package:KDDockWidgets/PositionedWidget.dart';
+import 'package:KDDockWidgetsBindings/Bindings.dart';
 import 'package:KDDockWidgetsBindings/Bindings_KDDWBindingsCore.dart'
     as KDDWBindingsCore;
 import 'package:KDDockWidgetsBindings/Bindings_KDDWBindingsFlutter.dart'
@@ -18,6 +19,7 @@ import 'package:KDDockWidgetsBindings/Bindings_KDDWBindingsFlutter.dart'
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart' hide View;
 import 'package:flutter/material.dart' as material show TabBar;
+import 'package:flutter/rendering.dart';
 
 class TabBar extends KDDWBindingsFlutter.TabBar with View_mixin {
   late final KDDWBindingsCore.TabBar m_controller;
@@ -34,8 +36,60 @@ class TabBar extends KDDWBindingsFlutter.TabBar with View_mixin {
   }
 
   @override
-  onRebuildRequested() {
-    // print("TabBar: onRebuildRequested");
+  onRebuildRequested() {}
+
+  void tabsForTabBar(RenderBox rb, List<RenderBox> result) {
+    rb.visitChildren((child) {
+      final rbChild = child as RenderBox?;
+      if (rbChild != null) {
+        if (rbChild.runtimeType.toString() == "_TabLabelBarRenderer") {
+          rbChild.visitChildren((child) {
+            final tab = child as RenderBox?;
+            if (tab != null) {
+              result.add(tab);
+            }
+          });
+        } else {
+          tabsForTabBar(rbChild, result);
+        }
+      }
+    });
+  }
+
+  void dumpRenderBox(RenderBox rb, int level) {
+    print(
+        "${"".padLeft(level * 4)} $rb size=${rb.size} type=${rb.runtimeType.toString()}");
+
+    rb.visitChildren((child) {
+      final rbChild = child as RenderBox?;
+      if (rbChild != null) {
+        dumpRenderBox(rbChild, level + 1);
+      }
+    });
+  }
+
+  @override
+  int tabAt(QPoint localPos) {
+    final localX = localPos.x().toDouble();
+    final localY = localPos.y().toDouble();
+
+    final renderBox = widgetKey.currentContext?.findRenderObject() as RenderBox;
+    final windowCoords = renderBox.localToGlobal(Offset(localX, localY));
+
+    List<RenderBox> tabs = [];
+    tabsForTabBar(renderBox, tabs);
+
+    for (int i = 0; i < tabs.length; ++i) {
+      final tab = tabs[i];
+      final tabWindowPos = tab.localToGlobal(Offset.zero);
+
+      final tabWindowRect = Rect.fromLTWH(
+          tabWindowPos.dx, tabWindowPos.dy, tab.size.width, tab.size.height);
+
+      if (tabWindowRect.contains(windowCoords)) return i;
+    }
+
+    return -1;
   }
 }
 
