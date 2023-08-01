29709a886a2e3e2fab9b7549cd8119e652e3a79b
Sergio Martins <sergio.martins@kdab.com>
2023-08-01 23:41:28 +0100

    tests: Add includes_test
    Checks if the required includes are installed.
    To be built by CI with -DCMAKE_PREFIX_PATH=/path/to/installed/kddw
    
    Some includes are broken, they are being fixed.

---
+++ tests/includes_test/main.cpp
@@ -0,0 +1,57 @@
+/*
+  This file is part of KDDockWidgets.
+
+  SPDX-FileCopyrightText: 2020-2023 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+  Author: Sergio Martins <sergio.martins@kdab.com>
+
+  SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+
+// Tests that the includes are installed
+
+// #include <kddockwidgets/qtwidgets/Action.h>
+#include <kddockwidgets/qtwidgets/Platform.h>
+#include <kddockwidgets/qtwidgets/ViewFactory.h>
+#include <kddockwidgets/qtwidgets/Window.h>
+#include <kddockwidgets/qtwidgets/views/ClassicIndicatorsWindow.h>
+#include <kddockwidgets/qtwidgets/views/DockWidget.h>
+#include <kddockwidgets/qtwidgets/views/DropArea.h>
+#include <kddockwidgets/qtwidgets/views/FloatingWindow.h>
+#include <kddockwidgets/qtwidgets/views/Group.h>
+#include <kddockwidgets/qtwidgets/views/MainWindow.h>
+#include <kddockwidgets/qtwidgets/views/MainWindowMDI.h>
+#include <kddockwidgets/qtwidgets/views/MDILayout.h>
+#include <kddockwidgets/qtwidgets/views/RubberBand.h>
+#include <kddockwidgets/qtwidgets/views/Separator.h>
+#include <kddockwidgets/qtwidgets/views/Stack.h>
+#include <kddockwidgets/qtwidgets/views/TabBar.h>
+#include <kddockwidgets/qtwidgets/views/TitleBar.h>
+#include <kddockwidgets/qtwidgets/views/View.h>
+// #include <kddockwidgets/qtwidgets/views/ViewWrapper.h>
+
+#include <kddockwidgets/qtquick/Action.h>
+#include <kddockwidgets/qtquick/Platform.h>
+#include <kddockwidgets/qtquick/ViewFactory.h>
+#include <kddockwidgets/qtquick/Window.h>
+#include <kddockwidgets/qtquick/views/ClassicIndicatorsWindow.h>
+#include <kddockwidgets/qtquick/views/DockWidget.h>
+#include <kddockwidgets/qtquick/views/DropArea.h>
+#include <kddockwidgets/qtquick/views/FloatingWindow.h>
+// #include <kddockwidgets/qtquick/views/Group.h>
+#include <kddockwidgets/qtquick/views/MainWindow.h>
+#include <kddockwidgets/qtquick/views/MainWindowMDI.h>
+#include <kddockwidgets/qtquick/views/MDILayout.h>
+#include <kddockwidgets/qtquick/views/RubberBand.h>
+#include <kddockwidgets/qtquick/views/Separator.h>
+#include <kddockwidgets/qtquick/views/Stack.h>
+// #include <kddockwidgets/qtquick/views/TabBar.h>
+#include <kddockwidgets/qtquick/views/TitleBar.h>
+#include <kddockwidgets/qtquick/views/View.h>
+// #include <kddockwidgets/qtquick/views/ViewWrapper.h>
+
+int main()
+{
+    return 0;
+}
