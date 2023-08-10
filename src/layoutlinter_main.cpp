0e16772d604642e19ea6e9d3efc5deff8d898db7
Sergio Martins <sergio.martins@kdab.com>
2023-08-10 18:24:13 +0100

    linter can now work on more than 1 layout

--- src/layoutlinter_main.cpp
+++ src/layoutlinter_main.cpp
@@ -19,9 +19,28 @@
 #include <QDebug>
 #include <QString>
 #include <QGuiApplication>
+#include <QCommandLineParser>
 
 using namespace KDDockWidgets;
 using namespace KDDockWidgets::Core;
+
+struct LinterConfig
+{
+    QStringList filesToLint;
+
+    bool isEmpty() const
+    {
+        return filesToLint.isEmpty();
+    }
+};
+
+LinterConfig requestedLinterConfig(const QCommandLineParser &parser)
+{
+    LinterConfig c;
+    c.filesToLint = parser.positionalArguments();
+
+    return c;
+}
 
 static bool lint(const QString &filename, RestoreOptions options)
 {
@@ -44,22 +63,34 @@ int main(int argc, char *argv[])
 {
     const auto frontends = Platform::frontendTypes();
     if (frontends.empty()) {
-        qDebug() << "Error: Your KDDockWidgets installation doesn't support any frontend!";
+        qWarning() << "Error: Your KDDockWidgets installation doesn't support any frontend!";
         return -1;
-    }
-
-    if (argc != 2) {
-        qDebug() << "Usage: kddockwidgets_linter <layout json file>";
-        return 1;
     }
 
     // Just take the 1st frontend, any is fine
     KDDockWidgets::Core::Platform::tests_initPlatform(argc, argv, frontends[0]);
 
+    QCommandLineParser parser;
+    parser.setApplicationDescription("KDDockWidgets layout linter");
+    parser.addHelpOption();
+    parser.addPositionalArgument("layout", "layout json file");
+    parser.process(*qApp);
+
+    const LinterConfig lc = requestedLinterConfig(parser);
+
+    if (lc.isEmpty()) {
+        qWarning() << "Expected a layout file";
+        return 3;
+    }
+
     const auto options = RestoreOption_None;
-    const int exitCode = lint(qGuiApp->arguments().at(1), options) ? 0 : 2;
+
+    int exitCode = 0;
+    for (const QString &layout : lc.filesToLint) {
+        if (!lint(qGuiApp->arguments().at(1), options))
+            exitCode = 2;
+    }
 
     KDDockWidgets::Core::Platform::tests_deinitPlatform();
-
     return exitCode;
 }
