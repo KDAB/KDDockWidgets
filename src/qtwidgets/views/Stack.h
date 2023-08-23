ffc9d59adb1910b2e1eb0f800f3115f846167173
Sergio Martins <sergio.martins@kdab.com>
2023-08-23 11:08:48 +0100

    Don't make Stack::init() final
    It's not called from ctor, so it's safe for user to override further

--- src/qtwidgets/views/Stack.h
+++ src/qtwidgets/views/Stack.h
@@ -31,7 +31,7 @@ public:
     QTabBar *tabBar() const;
 
     bool isPositionDraggable(QPoint p) const override;
-    void init() override final;
+    void init() override;
     void setDocumentMode(bool) override;
 
     /// @brief Returns the controller
