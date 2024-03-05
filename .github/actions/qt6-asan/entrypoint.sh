c8b4e4a2ea8c2ba76f50b77833b58bf7c04a3f03
Sergio Martins <sergio.martins@kdab.com>
2024-03-05 23:22:19 +0000

    ci: Add some debugging to container entrypoint
    GitHub says it can't find Qt

--- .github/actions/qt6-asan/entrypoint.sh
+++ .github/actions/qt6-asan/entrypoint.sh
@@ -3,6 +3,11 @@
 # SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
 #
 # SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only
-
+pwd
+ls
 cd /github/workspace/
+ls
+echo "Qt dir has"
+ls /Qt6/
+echo "Path is ${PATH}"
 cmake --preset=ci-dev-asan-qt6 -DCMAKE_PREFIX_PATH=/Qt6 && cd build-ci-dev-asan-qt6/ && ninja && ctest -j12
