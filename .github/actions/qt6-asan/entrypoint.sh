f166e43052666a8268c05d91346da7c160192dcc
Sergio Martins <sergio.martins@kdab.com>
2024-03-06 00:05:20 +0000

    ci: Don't run leak check with ASAN
    We only want it for the address sanitizer. We already have a workflow
    for leaks.
    
    Seems the envs don't propagate from the original docker file.

--- .github/actions/qt6-asan/entrypoint.sh
+++ .github/actions/qt6-asan/entrypoint.sh
@@ -3,11 +3,6 @@
 # SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
 #
 # SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only
-pwd
-ls
 cd /github/workspace/
-ls
-echo "Qt dir has"
-ls /Qt6/
-echo "Path is ${PATH}"
-cmake --preset=ci-dev-asan-qt6 -DCMAKE_PREFIX_PATH=/Qt6 && cd build-ci-dev-asan-qt6/ && ninja && ctest -j12
+env
+cmake --preset=ci-dev-asan-qt6 -DCMAKE_PREFIX_PATH=/Qt6 && cd build-ci-dev-asan-qt6/ && ninja && ctest -j4
