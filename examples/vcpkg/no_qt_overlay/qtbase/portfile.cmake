66c230c7fcf24f78bceae5e6d1034a68d9d1b92d
Sergio Martins <sergio.martins@kdab.com>
2024-12-03 13:29:04 +0000

    Fix reuse compliance
    Some files were missing in REUSE.toml

--- examples/vcpkg/no_qt_overlay/qtbase/portfile.cmake
+++ examples/vcpkg/no_qt_overlay/qtbase/portfile.cmake
@@ -1,1 +1,4 @@
+# SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+# SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only
+
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
