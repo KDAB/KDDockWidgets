aba9054330c0b01bbf1a3f750620cc46d61dccf6
Sergio Martins <sergio.martins@kdab.com>
2025-01-17 18:53:45 +0000

    flutter: Add set_guest_host()

--- src/flutter/kddw_bindings.h
+++ src/flutter/kddw_bindings.h
@@ -26,6 +26,7 @@ DOCKS_EXPORT void *create_host();
 DOCKS_EXPORT void delete_host(void *host);
 DOCKS_EXPORT void *create_guest(void *host, void (*callback)(void *guest, int x, int y, int width, int height, int is_visible));
 DOCKS_EXPORT void delete_guest(void *host);
+DOCKS_EXPORT void set_guest_host(void *host, void *guest);
 DOCKS_EXPORT void insert_item(void *host, void *guest, int location);
 DOCKS_EXPORT void insert_item_relative_to(void *host, void *guest, void *relativeToGuest, int location);
 DOCKS_EXPORT void remove_guest(void *host, void *guest);
