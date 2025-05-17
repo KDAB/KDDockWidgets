dba480a5a462a41188b0bcf91b54cba0557617bc
Sergio Martins <sergio.martins@kdab.com>
2025-05-17 19:11:48 +0100

    Support window managers without translucency in Qt 6 (#630)
    * Support window managers without translucency in Qt 6
    
    QX11Extras still exists in Qt 6, it's just private API
    
    For #629
    
    * update changelog
    
    * ci: Add qtx11extras_p.h to asan docker image
    
    Added a tone of x11 dependencies until configure accepted -xcb
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ .github/actions/qt6-asan/build_docker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This file is part of KDDockWidgets.
+#
+# SPDX-FileCopyrightText: 2025 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+# Author: Sérgio Martins <sergio.martins@kdab.com>
+#
+# SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only
+#
+# Contact KDAB at <info@kdab.com> for commercial licensing options.
+
+set -e
+
+SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
+
+cd $SCRIPT_PATH
+docker build -t kddw-qt6-asan -f Dockerfile_qtasan .
+
+CONTAINER_ID=$(docker run -d -it kddw-qt6-asan bash)
+echo "Container started with ID: $CONTAINER_ID"
+
+echo "exporting..."
+docker export -o mycontainer.tar $CONTAINER_ID
+
+echo "importing..."
+IMPORTED_IMAGE=$(docker import mycontainer.tar)
+
+echo "tagging.. $IMPORTED_IMAGE"
+docker tag $IMPORTED_IMAGE iamsergio/kddw-qt6-asan
+
+echo "pushing..."
+docker push iamsergio/kddw-qt6-asan
+
+echo "killing..."
+docker kill $CONTAINER_ID
+rm mycontainer.tar
